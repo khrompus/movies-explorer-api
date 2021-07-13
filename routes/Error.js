b635b124367db8bf933e94bffaeedde756351cbe
Vlad Rakhmaev <khrompus@yandex.ru>
2021-07-13 18:57:40 +0500

    initial commit

--- routes/Error.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const NotFoundError = require('../errors/NotFoundError');
-
-// eslint-disable-next-line no-unused-vars
-router.all('*', (req, res) => {
-  throw new NotFoundError('Такой страницы не существует');
-});
-
-module.exports = router;
