95683a7ec0050e118e00d327979dbbe70fcdc3b7
Vlad Rakhmaev <khrompus@yandex.ru>
2021-07-14 19:00:37 +0500

    feat: cors, ssl, yandexCloud, README.md, rate limit

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # movies-explorer-api
+Backend для сайта movies-explorer
+
+ipv4 = 193.32.218.197
+
+domains = khrompus-diploma-api.nomoredomains.club // api
+
+domains = khrompus-diploma-frontend.nomoredomains.club // frontend , еще не подключен
+
+p.s: api размещено на поддомене.
--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const router = require('./routes/index');
 const errorRouter = require('./routes/Error');
 const { requestLogger, errorLogger } = require('./middlewares/Logger');
 const { settingCors } = require('./utils/const');
+const { limiter } = require('./utils/rateLimiter');
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -23,9 +24,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(requestLogger);
+app.use(limiter);
 app.use(router);
 router.use('/', errorRouter);
-
 app.use(errorLogger);
 app.use(errors());
 app.use((err, req, res, next) => {
